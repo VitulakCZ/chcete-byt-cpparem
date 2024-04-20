fd0e38657eceb2dbfb3f4732609b3282164c1486
VitulakCZ <stemberk.vit@gmail.com>
2024-04-20 07:24:34 +0200

    Template

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include <vector>
 #include <cctype>
 
 using namespace std;
 const int POCET_OTAZEK = 15;
+const int POCET_ODPOVEDI = 4;
+
 class Otazka {
     private:
         string otazka;
-        vector<string> odpovedi;
+        string odpovedi[POCET_ODPOVEDI];
+        //vector<string> odpovedi;
         char spravnaOdpoved;
     public:
-        Otazka(string otazka, const string odpovedi[], char spravnaOdpoved) {
+        Otazka(const string otazka, const string odpovedi[], const char spravnaOdpoved) {
             this->otazka = otazka;
-            for (int i = 0; i < 4; i++)
-                this->odpovedi.push_back(odpovedi[i]);
+            for (int i = 0; i < POCET_ODPOVEDI; i++)
+                this->odpovedi[i] = odpovedi[i];
+            //for (int i = 0; i < 4; i++)
+            //    this->odpovedi.push_back(odpovedi[i]);
             this->spravnaOdpoved = spravnaOdpoved;
         }
 
@@ -26,35 +30,49 @@ class Otazka {
         char returnSpravnaOdpoved() {
             return spravnaOdpoved;
         }
+
         void printOdpovedi() {
             for (string odpoved : odpovedi) {
-                cout << odpoved << ((odpoved != odpovedi[odpovedi.size() - 1]) ? "\n" : ": ");
+                cout << odpoved << ((odpoved != odpovedi[POCET_ODPOVEDI - 1]) ? "\n" : ": ");
                 this_thread::sleep_for(chrono::milliseconds(100));
             }
         }
 };
 
-const string odpovedi[POCET_OTAZEK][4] = {
-    {"a) Ano", "b) Ne", "c) Nevim", "d) Tvoje máma je kokot!"},
-    {"a) Ano 2", "b) Ne 2", "c) Nevim 2", "d) Tvoje máma je kokot 2!"}
+const string odpovedi[POCET_OTAZEK][POCET_ODPOVEDI] = {
+    {"a) Napíše Hello, world!", "b) Čeká na input, poté napíše Hello, world!", "c) Je kompatibilní s C", "d) Hodí seg fault"},
+    {"a) 012", "b) 122", "c) 013", "d) 112"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"},
+    {"a) Odpověď 1", "b) Odpověď 2", "d) Odpověď 3", "d) Odpověď 4"}
 };
 
 const Otazka listOtazek[POCET_OTAZEK] = {
-    Otazka("Co udělá tento program?\n\n#include <iostream>\n\nusing namespace std;\nint main() {\n    cout << \"Hello, world!\";\n}", odpovedi[0], 'a'),
-    Otazka("Test č. 2:", odpovedi[1], 'b'),
-    Otazka("Test č. 3:", odpovedi[1], 'b'),
-    Otazka("Test č. 4:", odpovedi[1], 'b'),
-    Otazka("Test č. 5:", odpovedi[1], 'b'),
-    Otazka("Test č. 6:", odpovedi[1], 'b'),
-    Otazka("Test č. 7:", odpovedi[1], 'b'),
-    Otazka("Test č. 8:", odpovedi[1], 'b'),
-    Otazka("Test č. 9:", odpovedi[1], 'b'),
-    Otazka("Test č. 10:", odpovedi[1], 'b'),
-    Otazka("Test č. 11:", odpovedi[1], 'b'),
-    Otazka("Test č. 12:", odpovedi[1], 'b'),
-    Otazka("Test č. 13:", odpovedi[1], 'b'),
-    Otazka("Test č. 14:", odpovedi[1], 'b'),
-    Otazka("Test č. 15:", odpovedi[1], 'b')
+    Otazka("Co je pravda o programu:\n\n#include <iostream>\n\nusing namespace std;\nint main() {\n    cout << \"Hello, world!\" << endl;\n}", odpovedi[0], 'a'),
+    Otazka("Co napíše tento program?\n\n#include <iostream>\n\nusing namespace std;\nint main() {\n    int i = 0;\n    cout << ++i << i++ << i << endl;" , odpovedi[1], 'd'),
+    Otazka("Test č. 3:", odpovedi[2], 'c'),
+    Otazka("Test č. 4:", odpovedi[3], 'b'),
+    Otazka("Test č. 5:", odpovedi[4], 'b'),
+    Otazka("Test č. 6:", odpovedi[5], 'a'),
+    Otazka("Test č. 7:", odpovedi[6], 'd'),
+    Otazka("Test č. 8:", odpovedi[7], 'c'),
+    Otazka("Test č. 9:", odpovedi[8], 'c'),
+    Otazka("Test č. 10:", odpovedi[9], 'a'),
+    Otazka("Test č. 11:", odpovedi[10], 'c'),
+    Otazka("Test č. 12:", odpovedi[11], 'b'),
+    Otazka("Test č. 13:", odpovedi[12], 'b'),
+    Otazka("Test č. 14:", odpovedi[13], 'b'),
+    Otazka("Test č. 15:", odpovedi[14], 'd')
 };
 
 bool otazky(int index) {
@@ -87,11 +105,11 @@ int main() {
             break;
         }
         cout << "Ano správně, je to tak!" << endl;
-        this_thread::sleep_for(chrono::milliseconds(800));
+        this_thread::sleep_for(chrono::milliseconds(1200));
 
     }
     if (vyhra) {
-        cout << "Právě jste vyhrál hru chcete být c++ářem, GRATULUJEME!!!";
+        cout << "Právě jste vyhrál hru chcete být c++ářem, GRATULUJEME!!!" << endl;
         this_thread::sleep_for(chrono::milliseconds(3000));
     }
 }
